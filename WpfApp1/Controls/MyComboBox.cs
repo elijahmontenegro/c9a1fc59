7df525cf3e87a21d2a120c9a356e657e2b2d7ad1
b8394edf <ejmonte@outlook.com>
2018-05-28 02:47:35 -0500

    02.47 5.28.2018

---
+++ WpfApp1/Controls/MyComboBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Controls
+{
+    public class MyComboBox : ComboBox
+    {
+        public static readonly DependencyProperty m_ContentProperty;
+
+        static MyComboBox()
+        {
+            /////////////////////////////////////////////////////////////////////////////////
+            // Default Values
+            /////////////////////////////////////////////////////////////////////////////////
+            HeightProperty.OverrideMetadata(typeof(MyComboBox), new FrameworkPropertyMetadata(Convert.ToDouble(48)));
+            WidthProperty.OverrideMetadata(typeof(MyComboBox), new FrameworkPropertyMetadata(Convert.ToDouble(128)));
+            HorizontalContentAlignmentProperty.OverrideMetadata(typeof(MyComboBox), new FrameworkPropertyMetadata(HorizontalAlignment.Center));
+            VerticalContentAlignmentProperty.OverrideMetadata(typeof(MyComboBox), new FrameworkPropertyMetadata(VerticalAlignment.Center));
+
+            /////////////////////////////////////////////////////////////////////////////////
+            // Content Support
+            /////////////////////////////////////////////////////////////////////////////////
+            m_ContentProperty = DependencyProperty.Register("ContentProperty", typeof(object), typeof(MyComboBox), null);
+        }
+
+        public object ContentProperty
+        {
+            get { return GetValue(m_ContentProperty); }
+            set { SetValue(m_ContentProperty, value); }
+        }
+
+    }
+}
